a6372c3897dc96d3c7b1ab1d4308a398293887b6
olena2222 <olena.bosa@gmail.com>
2026-07-08 00:01:50 +0300

    fix3

--- PageObjects/HomePage.js
+++ PageObjects/HomePage.js
@@ -1,10 +1,10 @@
 import BasePage from "./BasePage.js"
-import Signup from "../Components/Signup.js"
+import Signup from "../Components/SignUp.js"
 import Login from "../Components/Login.js";
 export default class extends BasePage {
     constructor(page) {
         super(page, "/")
-        this.signup = new Signup(page);
+        this.signup = new SignUp(page);
         this.signin=new Login(page);
     }
 }
